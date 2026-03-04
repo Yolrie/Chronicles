30524df4666c704cbc3defc181ca8b66be013a1d
Quentin GERGAUD <quentin.gergaud44450@gmail.com>
2026-03-04 12:21:16 +0100

    Refactor App component to display initial project information and update navigation dependencies

--- App.tsx
+++ App.tsx
@@ -1,13 +1,26 @@
 // App.tsx
 
 import React from 'react';
-import { AuthProvider } from './src/contexts/AuthContext';
-import AppNavigator from './src/navigation/AppNavigator';
+import { View, Text, StyleSheet } from 'react-native';
 
 export default function App() {
   return (
-    <AuthProvider>
-      <AppNavigator />
-    </AuthProvider>
+    <View style={styles.container}>
+      <Text style={styles.title}>HSC - V2</Text>
+      <Text>Première version propre du projet.</Text>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 8,
+  },
+});
