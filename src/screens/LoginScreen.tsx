406ef5f35ce40631dac1df0f9e7802774333ae76
Quentin GERGAUD <quentin.gergaud44450@gmail.com>
2026-03-06 13:52:41 +0100

    Enhance LoginScreen with username input and update button title; improve user greeting display

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,24 +1,60 @@
 // LoginScreen.tsx
 
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, TextInput } from 'react-native';
 import commonStyles from '../styles/common';
+import { useState } from 'react';
+
+
 
 const LoginScreen = ({ navigation }) => {
+  
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState('');
+
   return (
     <View style={commonStyles.screen}>
       <View style={commonStyles.card}>
         <Text style={commonStyles.title}>LoginScreen</Text>
+
+        {username.length > 0 && (
         <Text style={commonStyles.subtitle}>
-          Connecte-toi pour accéder à l’accueil
+          Bonjour, {username}
         </Text>
+      )}
+
+
+        <TextInput
+            placeholder="Name"
+            placeholderTextColor="#9ca3af"
+            value={username}
+            onChangeText={setUsername}
+            style={{ color: '#e5e7eb' }}
+        />
+
+        <TextInput
+            placeholder="Email"
+            placeholderTextColor="#9ca3af"
+            value={email}
+            onChangeText={setEmail}
+            style={{ color: '#e5e7eb' }}
+        />
+
+        <TextInput
+            placeholder="Mot de passe"
+            placeholderTextColor="#9ca3af"
+            value={password}
+            onChangeText={setPassword}
+            style={{ color: '#e5e7eb' }}
+        />
 
         <View style={commonStyles.actions}>
           <Button
             onPress={() => {
               navigation.navigate('Home');
             }}
-            title="Aller à Home Screen"
+            title="Enregistrer"
           />
         </View>
       </View>
